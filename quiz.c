a66d130026a447572ce5f5e1ed91b6e6b175ec61
Abhaysingh04 <110380236+Abhaysingh04@users.noreply.github.com>
2022-08-20 22:10:22 +0530

    learning

---
+++ quiz.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+int main() 
+{
+  char questions[][100] = {"1. Name the dengue mosquitos?:",
+                           "2. Who cause elephantisis?:",
+                           "3. World heredity day?:"};
+
+  char answers[][100] = {"A. Aedes Egypti", "B. Culex", "C. Elphantisis", "D.Anolpeles",
+                         "A. abhay", "B. Culex", "C. Elphantisis", "D.Anolpex",
+                         "A. 7th April", "B. Culex", "C. Elphant", "D.11st",};
+                        
+
+
+                                                   
+
+
+
+  \n", questions[i]);
+       
+    
+
+
+    return 0;
+}                                                               
+                                                               
+                                                               
+                                                               
+                                                               
+                                                               
+                                                               
+                                                               
+                                                               
+                                                               
+                                                               
+
+
+
+
+
+
+
+
+
+
+
+
