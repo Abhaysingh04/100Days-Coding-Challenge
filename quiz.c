b68979a81670066a1a33d0ec5daa5e80f386f234
Abhaysingh04 <110380236+Abhaysingh04@users.noreply.github.com>
2022-08-22 20:36:06 +0530

    code

--- quiz.c
+++ quiz.c
@@ -3,27 +3,23 @@
 
 int main() 
 {
-  char questions[][100] = {"1. Name the dengue mosquitos?:",
-                           "2. Who cause elephantisis?:",
-                           "3. World heredity day?:"};
+    char questions[][100] ={"1. Name the dengue mosquitos?:\n",
+                           "2. Who cause elephantisis?:\n",
+                           "3. World heredity day?:\n"};
 
-  char answers[][100] = {"A. Aedes Egypti", "B. Culex", "C. Elphantisis", "D.Anolpeles",
-                         "A. abhay", "B. Culex", "C. Elphantisis", "D.Anolpex",
-                         "A. 7th April", "B. Culex", "C. Elphant", "D.11st",};
+    char answers[][100] ={"A. Aedes Egypti", "B. Culex", "C. Elphantisis", "D.Anolpeles\n",
+                         "A. abhay", "B. Culex", "C. Elphantisis", "D.Anolpex\n",
+                         "A. 7th April", "B. Culex", "C. Elphant", "D.11th \n"};
                         
-
+  printf("%c", &questions);
 
                                                    
 
 
 
-  \n", questions[i]);
-       
     
-
-
-    return 0;
-}                                                               
+  return 0;
+}
                                                                
                                                                
                                                                
