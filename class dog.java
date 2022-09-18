5cfdca6c8736b24a155f0a91a0194b471d41886c
Abhaysingh04 <110380236+Abhaysingh04@users.noreply.github.com>
2022-09-18 19:33:56 +0530

    java

---
+++ class dog.java
@@ -0,0 +1,27 @@
+class dog
+{
+    string colour;
+    string breed;
+    string age;
+
+    public void running()
+    {
+        system.out.println("its running fast");
+    }
+}
+public dogfeature()
+{
+    public static void main ( string args[]){
+        dog dog1 = new dog ();
+        dog1.colour = "red";
+        dog1.breed = "labour";
+        dog1.age =" black";
+
+        dog1.running();
+    }
+
+}
+
+
+
+
